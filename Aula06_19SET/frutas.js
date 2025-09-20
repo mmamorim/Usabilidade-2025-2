9cac8a4dc395e389d0088966c1ab6f21fcf71ae2
Marcelo <marc.m.amorim@gmail.com>
2025-09-19 21:24:21 -0300

    v1

---
+++ Aula06_19SET/frutas.js
@@ -0,0 +1,10 @@
+
+export default function bomDia() {
+    console.log("Bom dia!");
+}
+
+//let fruta1 = "Maçã" 
+//let fruta2 = "Pera" 
+//let fruta3 = "Pera" 
+//let fruta4 = "Pera" 
+
---
+++ Aula06_19SET/main.js
@@ -0,0 +1,16 @@
+import { v4 as uuidv4 } from 'uuid';
+import ola from "./frutas.js"
+
+console.log("oi gente");
+
+let algo = (x) => x == 10
+
+
+let id = uuidv4()
+console.log(id);
+
+ola()
+//console.log(umaFruta);
+//console.log(fruta2);
+
+
