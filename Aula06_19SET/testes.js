92fa2c5d9e441a3010e3164ee6af1414f9dabad5
Marcelo <marc.m.amorim@gmail.com>
2025-10-04 09:12:00 -0300

    v1

---
+++ Aula06_19SET/testes.js
@@ -0,0 +1,19 @@
+
+
+console.log("passei aqui")
+
+function fazAlgo(funcCalcula) {
+    console.log("estou fazendo algo....");
+    funcCalcula(5,6)
+}
+
+fazAlgo((x,y) => {
+    console.log("saoma de x + y",x+y);   
+})
+
+app.get("/produtos", (req,res) => {
+    dfdfdf
+    dfdfdfdf
+
+    dfdfdf
+})
